889824c07eb8ff9ea8bb896abf4756920480a16b
Riley <riley@rileybathurst.com>
2023-05-31 09:48:22 -0700

    pnpm

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,12 +9,24 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
+  plugins: [
+    {
+      resolve: `gatsby-plugin-postcss`,
+      options: {
+        postCssPlugins: [
+          require(`postcss-import`),
+          require('autoprefixer'),
+          require('postcss-nested'),
+        ],
+      },
+    },
+    "gatsby-plugin-sitemap", {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
     }
-  }]
+  ]
 };
 
 export default config;
